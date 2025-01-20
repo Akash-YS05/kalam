480605dd0f016d27191e5ef3558bc512be3ccf13
Akash-YS05 <akash.pan05@gmail.com>
2025-01-20 10:00:01 +0530

    added db

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -12,6 +12,11 @@ wss.on('connection', function connection(ws, request) {
     const params = new URLSearchParams(url.split('?')[1]);
     const token = params.get('token') || '';    //extract the token from the url
     const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (typeof decoded === 'string') {
+        ws.close();
+        return;
+    }
     
     if (!decoded || !(decoded as JwtPayload).userId) {
         ws.close();
