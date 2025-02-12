30cda289b74a18f0ded0901339ced2f1e1f7e7b5
Akash-YS05 <akash.pan05@gmail.com>
2025-02-12 13:54:37 +0530

    bugs fix

--- apps/kalam-frontend/app/layout.tsx
+++ apps/kalam-frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Kalam, Satisfy, Gotu, Tillana } from "next/font/google";
+import { Geist, Geist_Mono, Satisfy, Gotu } from "next/font/google";
 import "./globals.css";
 
 const gotu = Gotu({
