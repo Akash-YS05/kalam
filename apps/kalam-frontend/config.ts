e9049bfcc479174eff368aa21efe555ce7b56b11
Akash-YS05 <akash.pan05@gmail.com>
2025-05-23 11:10:31 +0530

    url update

--- apps/kalam-frontend/config.ts
+++ apps/kalam-frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_URL = "http://localhost:3001";
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_URL = "https://kalam-http.onrender.com";
+export const WS_URL = "https://kalam-zm81.onrender.com";
