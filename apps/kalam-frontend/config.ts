ba6b42154395c84d92f9a6ffa6396dd1ffd85d5e
Akash-YS05 <akash.pan05@gmail.com>
2025-07-08 18:01:42 +0530

    config

--- apps/kalam-frontend/config.ts
+++ apps/kalam-frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_URL = process.env.HTTP_URL || "http://localhost:3002";
-export const WS_URL = process.env.WEBSOCKET_URL || "ws://localhost:8080";
+export const HTTP_URL = process.env.NEXT_PUBLIC_HTTP_URL || "http://localhost:3002";
+export const WS_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || "ws://localhost:8080";
