088860bb898eb04bb23bd18d4870b30ab3bd3ead
Akash-YS05 <akash.pan05@gmail.com>
2025-07-18 22:14:46 +0530

    metadata

--- apps/kalam-frontend/app/layout.tsx
+++ apps/kalam-frontend/app/layout.tsx
@@ -26,9 +26,50 @@ const devanagri = Noto_Serif_Devanagari({
   subsets: ["devanagari"],
   variable: "--font-devanagari",
 });
-export const metadata: Metadata = {
-  title: "Kalam",
+
+export const metadata = {
+  title: "Kalam – Digital Whiteboard for Brainstorming & Collaboration",
+  description:
+    "Kalam is a fast, interactive digital whiteboard designed for seamless brainstorming, diagramming, and real-time collaboration. Enhance productivity and creativity with an intuitive canvas.",
+  keywords: [
+    "Kalam",
+    "digital whiteboard",
+    "online whiteboard",
+    "brainstorming tool",
+    "real-time collaboration",
+    "diagramming app",
+    "team productivity",
+    "interactive whiteboard",
+    "collaborative workspace"
+  ],
+  authors: [{ name: "Akash", url: "https://akassh.tech" }],
+  creator: "Akash Pandey",
+  openGraph: {
+    title: "Kalam – Brainstorm, Diagram, and Collaborate in Real-Time",
+    description:
+      "Experience a seamless and interactive digital whiteboard with Kalam. Perfect for teams looking to brainstorm, diagram, and collaborate efficiently.",
+    url: "https://kalamm.vercel.app",
+    siteName: "Kalam",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddkt00y0i/image/upload/v1752856969/gradii-1920x1080_10_flremb.png", 
+        width: 1200,
+        height: 630,
+        alt: "Kalam – Digital Whiteboard",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kalam – Digital Whiteboard for Collaboration",
+    description:
+      "Kalam is a real-time digital whiteboard for brainstorming, diagramming, and team collaboration.",
+    images: ["https://res.cloudinary.com/ddkt00y0i/image/upload/v1752856969/gradii-1920x1080_10_flremb.png"],
+    creator: "@akashpandeytwt",
+  },
 };
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
